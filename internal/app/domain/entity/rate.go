64480afbd514c8aef8b9f6b00d2395e0c93a6357
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-04-19 20:47:32 +0400

    chore: add field

--- internal/app/domain/entity/rate.go
+++ internal/app/domain/entity/rate.go
@@ -3,7 +3,8 @@ package entity
 import "math/big"
 
 type Rate struct {
-	Quote string
-	Base  string
-	Price *big.Float
+	Quote    string     `json:"quote"`
+	Base     string     `json:"base"`
+	Price    *big.Float `json:"price"`
+	Provider string     `json:"provider"`
 }
