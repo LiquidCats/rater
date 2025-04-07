8ee5f7f7799633cc3d764cce75ecc961aa2feae7
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-04-07 22:57:26 +0400

    deps(): redefine domain entities

--- internal/port/logger.go
+++
@@ -1,9 +0,0 @@
-package port
-
-import "go.uber.org/zap"
-
-type Logger interface {
-	Debug(msg string, fields ...zap.Field)
-	Info(msg string, fields ...zap.Field)
-	Error(msg string, fields ...zap.Field)
-}
