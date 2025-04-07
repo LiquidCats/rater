94f15da55d4413eba3c3f1a2460012978ba2d363
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-04-07 22:32:14 +0400

    chore(): organize configs

--- configs/loader.go
+++ configs/loader.go
@@ -1,48 +1,15 @@
 package configs
 
 import (
-	"os"
-	"rater/internal/app/domain/types"
-	"strconv"
-	"strings"
+	"github.com/kelseyhightower/envconfig"
 )
 
-func Load() Config {
+func Load(prefix string) (Config, error) {
+	var cfg Config
 
-	cfg := Config{
-		Port: os.Getenv("RATER_PORT"),
-		Redis: RedisConfig{
-			Address:  os.Getenv("RATER_REDIS_ADDRESS"),
-			Password: os.Getenv("RATER_REDIS_PASSWORD"),
-		},
+	if err := envconfig.Process(prefix, &cfg); err != nil {
+		return cfg, err
 	}
 
-	loadRedisDB(&cfg)
-	loadBaseCurrencies(&cfg)
-	loadQuoteCurrencies(&cfg)
-
-	return cfg
-}
-
-func loadRedisDB(cfg *Config) {
-	redisDB, err := strconv.Atoi(os.Getenv("RATER_REDIS_DB"))
-	if nil != err {
-		redisDB = 0
-	}
-
-	cfg.Redis.DB = redisDB
-}
-
-func loadBaseCurrencies(cfg *Config) {
-	baseCurrencies := strings.Split(os.Getenv("RATER_BASE_CURRENCIES"), ",")
-	for _, currency := range baseCurrencies {
-		cfg.BaseCurrencies = append(cfg.BaseCurrencies, types.BaseCurrency(currency))
-	}
-}
-
-func loadQuoteCurrencies(cfg *Config) {
-	quoteCurrencies := strings.Split(os.Getenv("RATER_QUOTE_CURRENCIES"), ",")
-	for _, currency := range quoteCurrencies {
-		cfg.QuoteCurrencies = append(cfg.QuoteCurrencies, types.QuoteCurrency(currency))
-	}
+	return cfg, nil
 }
