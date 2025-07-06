311e74a10092fb487a596f06c8a13e43c14f02af
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-07-06 20:15:40 +0400

    chore: add gin-contrib/logger middleware to Prometheus handler

--- internal/adapter/metrics/prometheus/handler.go
+++ internal/adapter/metrics/prometheus/handler.go
@@ -1,6 +1,7 @@
 package prometheus
 
 import (
+	"github.com/gin-contrib/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -8,6 +9,7 @@ import (
 func GinHandler() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	mux := gin.New()
+	mux.Use(logger.SetLogger(logger.WithUTC(true)))
 
 	mux.Any("/metrics", func(c *gin.Context) {
 		promhttp.Handler().ServeHTTP(c.Writer, c.Request)
