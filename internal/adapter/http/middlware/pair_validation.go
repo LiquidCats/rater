f5389b57766a08bde8da12681995ec9be15c8472
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-07-19 23:00:38 +0400

    refactor: update PairValidationMiddleware to replace allowed pairs with PairDatabase, improve validation logic and tests

--- internal/adapter/http/middlware/pair_validation.go
+++ internal/adapter/http/middlware/pair_validation.go
@@ -1,57 +1,69 @@
 package middlware
 
 import (
+	"fmt"
 	"net/http"
-	"slices"
 	"strings"
 
 	"github.com/LiquidCats/rater/internal/adapter/http/dto"
-	"github.com/LiquidCats/rater/internal/app/domain/entity"
+	"github.com/LiquidCats/rater/internal/app/port/adapter/repository"
 	"github.com/gin-gonic/gin"
 )
 
 type PairValidationMiddleware struct {
-	allowed []entity.CurrencyPairString
+	db repository.PairDatabase
 }
 
-func NewPairValidation(allowed []entity.CurrencyPairString) *PairValidationMiddleware {
+func NewPairValidation(db repository.PairDatabase) *PairValidationMiddleware {
 	return &PairValidationMiddleware{
-		allowed: allowed,
+		db: db,
 	}
 }
 
 func (m *PairValidationMiddleware) Handle(ctx *gin.Context) {
 	pair := strings.TrimSpace(ctx.Param("pair"))
 
-	if len(m.allowed) == 0 {
-		m.setValidationError(ctx, "no allowed pairs")
+	count, err := m.db.CountPairs(ctx.Request.Context())
+	if err != nil {
+		setValidationError(ctx, fmt.Sprintf("pair validation failed (%s)", err.Error()))
+		return
+	}
+
+	if count == 0 {
+		setValidationError(ctx, "no allowed pairs")
 		return
 	}
 
 	if pair == "" {
-		m.setValidationError(ctx, "empty pair")
+		setValidationError(ctx, "empty pair")
 		ctx.Abort()
 		return
 	}
 	if len(pair) < 7 { // nolint:mnd
-		m.setValidationError(ctx, "should be 7 characters long at least")
+		setValidationError(ctx, "should be 7 characters long at least")
 		return
 	}
 
 	if pair != strings.ToUpper(pair) {
-		m.setValidationError(ctx, "should be uppercase")
+		setValidationError(ctx, "should be uppercase")
 		return
 	}
 
-	if !slices.Contains(m.allowed, entity.CurrencyPairString(pair)) {
-		m.setValidationError(ctx, "not allowed")
+	exists, err := m.db.HasPair(ctx, strings.ToUpper(pair))
+	if err != nil {
+		setValidationError(ctx, fmt.Sprintf("pair validation failed (%s)", err.Error()))
+		return
+	}
+
+	if !exists {
+		setValidationError(ctx, "not allowed")
 		return
 	}
 
 	ctx.Next()
 }
 
-func (m *PairValidationMiddleware) setValidationError(ctx *gin.Context, text ...string) {
+func setValidationError(ctx *gin.Context, text ...string) {
 	ctx.AbortWithStatusJSON(
 		http.StatusUnprocessableEntity,
 		dto.NewValidationErrorResponse(
