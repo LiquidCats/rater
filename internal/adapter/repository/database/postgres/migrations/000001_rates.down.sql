923090d169b29f29b40ff82217b83b41878f7599
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-07-19 22:53:06 +0400

    refactor: restructure database schema and adjust domain entities

---
+++ internal/adapter/repository/database/postgres/migrations/000001_rates.down.sql
@@ -0,0 +1,3 @@
+drop table if exists rates;
+drop table if exists pairs;
+drop table if exists providers;
---
+++ internal/app/domain/entity/pair.go
@@ -0,0 +1,67 @@
+package entity
+
+import (
+	"fmt"
+	"strings"
+)
+
+type CurrencyISO string
+
+func (s CurrencyISO) ToUpper() CurrencyISO {
+	return CurrencyISO(strings.ToUpper(string(s)))
+}
+
+func (s CurrencyISO) ToLower() CurrencyISO {
+	return CurrencyISO(strings.ToLower(string(s)))
+}
+
+func (s CurrencyISO) String() string {
+	return string(s)
+}
+
+type Symbol string
+
+func (s Symbol) ToUpper() Symbol {
+	return Symbol(strings.ToUpper(string(s)))
+}
+
+func (s Symbol) ToLower() Symbol {
+	return Symbol(strings.ToLower(string(s)))
+}
+
+func (s Symbol) String() string {
+	return string(s)
+}
+
+func (s Symbol) ToPair() Pair {
+	parts := strings.Split(s.String(), "_")
+	return Pair{
+		From:   CurrencyISO(parts[0]).ToUpper(),
+		To:     CurrencyISO(parts[1]).ToUpper(),
+		Symbol: s,
+	}
+}
+
+func NewSymbol(from, to CurrencyISO) Symbol {
+	return Symbol(strings.ToUpper(fmt.Sprintf("%s_%s", from, to)))
+}
+
+type Pair struct {
+	From   CurrencyISO
+	To     CurrencyISO
+	Symbol Symbol
+}
+
+func (p *Pair) Join(glue string) string {
+	return fmt.Sprint(p.From, glue, p.To)
+}
+
+func (p *Pair) ToCacheKey() string {
+	return strings.ToLower(
+		fmt.Sprintf(
+			"from:%s:to:%s",
+			p.From,
+			p.To,
+		),
+	)
+}
