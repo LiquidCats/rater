716e4f099df86182fbb5b919e192c7dde42d5554
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-07-20 02:06:24 +0400

    feat: add RateService with methods for fetching current and historical rates

---
+++ internal/app/service/rate_service.go
@@ -0,0 +1,149 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/LiquidCats/rater/internal/adapter/repository/database/postgres"
+	"github.com/LiquidCats/rater/internal/app/bootstrap"
+	"github.com/LiquidCats/rater/internal/app/domain/entity"
+	domain "github.com/LiquidCats/rater/internal/app/domain/errors"
+	"github.com/LiquidCats/rater/internal/app/port/adapter/metrics"
+	"github.com/LiquidCats/rater/internal/app/port/adapter/repository"
+	"github.com/LiquidCats/rater/internal/app/utils/timeutils"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/rotisserie/eris"
+	"github.com/rs/zerolog"
+	"github.com/shopspring/decimal"
+)
+
+type RateService struct {
+	adapters   bootstrap.Registry
+	rateDB     repository.RateDatabase
+	errMetrics metrics.ProviderErrRateMetric
+}
+
+func NewRateService(
+	adapters bootstrap.Registry,
+	rateDB repository.RateDatabase,
+	errMetrics metrics.ProviderErrRateMetric,
+) *RateService {
+	return &RateService{
+		adapters:   adapters,
+		rateDB:     rateDB,
+		errMetrics: errMetrics,
+	}
+}
+
+func (s *RateService) Historical(ctx context.Context, pair entity.Pair, ts time.Time) (*entity.Rate, error) {
+	historical, err := s.rateDB.GetRate(ctx, postgres.GetRateParams{
+		Ts: pgtype.Timestamp{
+			Time:  ts,
+			Valid: true,
+		},
+		Pair: pair.Symbol.String(),
+	})
+	if err != nil {
+		if eris.Is(err, sql.ErrNoRows) {
+			return nil, domain.ErrNoHistoricalRate
+		}
+	}
+
+	rate := entity.NewRate(historical)
+
+	return &rate, nil
+}
+
+func (s *RateService) Current(ctx context.Context, pair entity.Pair) (*entity.Rate, entity.ProviderName, error) {
+	var (
+		err      error
+		price    decimal.Decimal
+		provider entity.ProviderName
+	)
+	start := time.Now()
+
+	logger := zerolog.Ctx(ctx).
+		With().
+		Str("name", "use_case.fetchRate").
+		Any("pair", pair).
+		Logger()
+
+	for name, adapter := range s.adapters {
+		price, err = adapter.GetRate(ctx, pair)
+		provider = name
+		if err != nil {
+			var providerErr *domain.ProviderRequestFailedError
+			if eris.As(err, &providerErr) {
+				logger = logger.With().
+					Int("provider_err_code", providerErr.StatusCode).
+					Str("provider_err_body", providerErr.Body).
+					Logger()
+				s.errMetrics.Inc(providerErr.StatusCode, name)
+			}
+
+			logger.Error().
+				Any("err", eris.ToJSON(err, true)).
+				Any("provider", name).
+				Msg("cant get rate")
+			continue
+		}
+
+		if !price.IsZero() {
+			break
+		}
+	}
+
+	if price.IsZero() {
+		s.errMetrics.Inc(-1, provider)
+
+		logger.Error().
+			Any("err", eris.ToJSON(err, true)).
+			Any("provider", provider).
+			Msg("rate not available")
+		return nil, provider, domain.ErrRateNotAvailable
+	}
+
+	rate := &entity.Rate{
+		Pair:     pair,
+		Price:    price,
+		Provider: provider,
+	}
+
+	if hasRate, err := s.rateDB.HasRate(ctx, postgres.HasRateParams{
+		Ts: pgtype.Timestamp{
+			Time:  timeutils.RoundToNearest(start, timeutils.FiveMinuteBucket),
+			Valid: true,
+		},
+		Pair: rate.Provider.String(),
+	}); err == nil && hasRate {
+		return rate, provider, nil
+	} else if err != nil {
+		logger.Warn().
+			Any("err", eris.ToJSON(err, true)).
+			Msg("cant check rate in database")
+		return rate, provider, nil
+	}
+
+	_, err = s.rateDB.SaveRate(ctx, postgres.SaveRateParams{
+		Price: pgtype.Numeric{
+			Int:   rate.Price.Coefficient(),
+			Exp:   rate.Price.Exponent(),
+			Valid: true,
+		},
+		Pair:     rate.Pair.Symbol.String(),
+		Provider: rate.Provider.String(),
+		Ts: pgtype.Timestamp{
+			Time:  timeutils.RoundToNearest(start, timeutils.FiveMinuteBucket),
+			Valid: true,
+		},
+	})
+
+	if err != nil {
+		logger.Warn().
+			Any("err", eris.ToJSON(err, true)).
+			Msg("cant save rate to database")
+	}
+
+	return rate, provider, nil
+}
