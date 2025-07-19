3bd5bd6d9a6afdd19e8eaa07276061bf4abeabbd
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-07-20 01:02:21 +0400

    refactor: add envconfig tags and defaults to RedisConfig fields for better environment configuration handling

--- configs/redis.go
+++ configs/redis.go
@@ -3,7 +3,8 @@ package configs
 import "github.com/go-playground/sensitive"
 
 type RedisConfig struct {
-	Address  string
-	Password sensitive.String
-	DB       int
+	Address  string           `envconfig:"ADDRESS"`
+	Password sensitive.String `envconfig:"PASSWORD"`
+	DB       int              `envconfig:"DB" default:"0"`
+	Protocol int              `envconfig:"PROTOCOL" default:"3"`
 }
