a0109cf7545dc942a4fe2ecd3becfc027b2db760
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-07-19 23:00:22 +0400

    refactor: improve AppConfig with new constants and fields, update secret handling logic and loader processing

--- configs/app.go
+++ configs/app.go
@@ -4,6 +4,10 @@ import (
 	"github.com/rs/zerolog"
 )
 
+const AppName = "rater"
+
 type AppConfig struct {
 	LogLevel zerolog.Level `envconfig:"LOG_LEVEL" default:"info"`
+
+	CollectSchedule string `envconfig:"COLLECT_SCHEDULE" default:"*/5 * * * *"`
 }
